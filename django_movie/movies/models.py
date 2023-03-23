9b144396ea9069aae711478408974466db5496cd
AlexFromSiberia <id764g@gmail.com>
2023-03-23 11:47:12 +0700

    Done:  - models.py

--- django_movie/movies/models.py
+++ django_movie/movies/models.py
@@ -1,7 +1,8 @@
+from datetime import date
 from django.db import models
 
 
-class Categories(models.Model):
+class Category(models.Model):
     """Категории фильмов"""
     name = models.CharField('Категория', max_length=100)
     description = models.TextField('Описание')
@@ -30,4 +31,99 @@ class Actor(models.Model):
         verbose_name_plural = 'Актёры и режиссеры'
 
 
+class Genre(models.Model):
+    """Жанры"""
+    name = models.CharField('Жанр', max_length=100)
+    description = models.TextField('Описание')
+    url = models.SlugField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Жанр'
+        verbose_name_plural = 'Жанры'
+
+
+class Movie(models.Model):
+    title = models.CharField('Название', max_length=100)
+    tagline = models.CharField('Слоган', max_length=300, default='')
+    poster = models.ImageField('Постер', upload_to='movies/')
+    year = models.PositiveSmallIntegerField('Дата выхода', default='2000')
+    country = models.CharField('Страна', max_length=40)
+    directors = models.ManyToManyField(Actor, verbose_name='режиссёр', related_name='film_director')
+    actors = models.ManyToManyField(Actor, verbose_name='актёр', related_name='film_actor')
+    genres = models.ManyToManyField(Genre, verbose_name='жанры')
+    world_premiere = models.DateField('Премьера в мире', default=date.today)
+    budget = models.PositiveSmallIntegerField('Бюджет', default=0, help_text='указывать сумму в долларах')
+    fees_in_usa = models.PositiveSmallIntegerField('Сборы в США', default=0, help_text='указывать сумму в долларах')
+    fees_in_world = models.PositiveSmallIntegerField('Сборы в мире', default=0, help_text='указывать сумму в долларах')
+    category = models.ForeignKey(Category, verbose_name='Категория', on_delete=models.SET_NULL, null=True)
+    url = models.SlugField(max_length=100, unique=True)
+    draft = models.BooleanField('Черновик', default=False)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Фильм'
+        verbose_name_plural = 'Фильмы'
+
+
+class MovieShots(models.Model):
+    """Кадры из фильма"""
+    title = models.CharField('Заголовок', max_length=100)
+    description = models.TextField('Описание')
+    image = models.ImageField('Изображение', upload_to="movie_shots/")
+    movie = models.ForeignKey(Movie, verbose_name='Фильм', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Кадр'
+        verbose_name_plural = 'Кадры'
+
+
+class RatingStar(models.Model):
+    """Звезда рейтинга"""
+    value = models.PositiveSmallIntegerField('Значение', default=0)
+
+    def __str__(self):
+        return self.value
+
+    class Meta:
+        verbose_name = 'Звезда рейтинга'
+        verbose_name_plural = 'Звезды рейтинга'
+
+
+class Rating(models.Model):
+    """Рейтинг"""
+    ip = models.CharField("IP address", max_length=15)
+    star = models.ForeignKey(RatingStar, on_delete=models.CASCADE, verbose_name='Звезда')
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE, verbose_name='Фильм')
+
+    def __str__(self):
+        return f'{self.star} - {self.movie}'
+
+    class Meta:
+        verbose_name = 'Рейтинг'
+        verbose_name_plural = 'Рейтинги'
+
+
+class Reviews(models.Model):
+    """Отзывы"""
+    email = models.EmailField()
+    name = models.CharField('Имя', max_length=100)
+    text = models.TextField('Сообщение', max_length=5000)
+    # 'self' - запись будет ссылаться на запись в этой же таблице ???
+    parent = models.ForeignKey('self', verbose_name='Родитель', on_delete=models.SET_NULL, blank=True, null=True)
+    movie = models.ForeignKey(Movie, verbose_name='фильм', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.name} - {self.movie}'
+
+    class Meta:
+        verbose_name = 'Отзыв'
+        verbose_name_plural = 'Отзывы'
 
